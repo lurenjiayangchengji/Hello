fbcffc92e03ead88927c9e4d48e9ddf0e81043da
lurenjiayangchengji <2188416651@qq.com>
2026-02-07 10:13:16 +0800

    Add files via upload

---
+++ Hell world.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+	printf("Hello world\n");
+	printf("Hope we can get a good end,I love computer.So I selet it.I will acquire more knowledge.I will keep going\n");
+	return 0;
+}
